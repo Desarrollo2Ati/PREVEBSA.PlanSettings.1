c795d5393c7b2cc6a800c11face017282253c20e
angiePedraza <minijin1234567@gmail.com>
2026-02-27 10:46:38 -0500

    ControllerGeneric

---
+++ PREVEBSA/PREVEBSAPlanSettings.Backend/Controllers/Settings/ActivityStepController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PREVEBSAPlanSettingBackend.Controllers;
+using PREVEBSAPlanSettingBackend.UnitsOfWork.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PREVEBSAPlanSettings.ClassLibrary.Entities.Settings;
+namespace PREVEBSAPlanSettings.Backend.Controllers.Settings
+{ 
+
+
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class ActivityStepController : GenericController<ActivityStep>
+    {
+        public ActivityStepController(IGenericUnitOfWork<ActivityStep> unitOfWork) : base(unitOfWork)
+        {
+
+        }
+    }
+}
---
+++ PREVEBSA/PREVEBSAPlanSettings.Backend/Controllers/Settings/ActivityStepRisksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PREVEBSAPlanSettingBackend.Controllers;
+using PREVEBSAPlanSettingBackend.UnitsOfWork.Interfaces;
+using PREVEBSAPlanSettings.ClassLibrary.Entities.Settings;
+
+namespace PREVEBSAPlanSettings.Backend.Controllers.Settings
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class ActivityStepRisksController : GenericController<ActivityStepRisks>
+    {
+        public ActivityStepRisksController(IGenericUnitOfWork<ActivityStepRisks> unitOfWork) : base(unitOfWork)
+        {
+
+        }
+    }
+}
